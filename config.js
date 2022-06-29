563df62986250a6a3fca89bf213c6bb06ee71ece
ramlaidi <83389072+ramlaidi@users.noreply.github.com>
2022-06-29 12:36:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455']
+global.owner = ['6282135677642', '855387869292']
 
 global.APIs = {
   amel: 'https://melcanz.com',
-}
+}.
 global.APIKeys = {
-  'https://melcanz.com': 'Subscribe MelcanzYT'
+  'https://melcanz.com': 'irsantod'
 }
 
-global.packname = 'ig @ariffb.id @melcanz.io'
-global.author = '© ariffb & melcanz'
-global.wm = '© ariffb & melcanz'
-global.eror = '*Error*'
-global.wait = '*tunggu..*'
+global.packname = 'Irsan Ganz'
+global.author = '© irsan & kraken'
+global.wm = '© irsan & kraken'
+global.eror = '*Error Mek*'
+global.wait = '*tunggu.. Asu*'
 global.benar = '✅'
 global.salah = '❌'
-global.dikit = 'dikit lagi'
+global.dikit = 'dikit lagi Gblk'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
 
 let file = require.resolve(__filename)
