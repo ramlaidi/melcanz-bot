4be70cc1685daa7aaca981512793725d0b3d491c
ramlaidi <83389072+ramlaidi@users.noreply.github.com>
2022-07-05 12:33:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455']
+global.owner = ['6281329869802', '6281329869802']
 
 global.APIs = {
   amel: 'https://melcanz.com',
